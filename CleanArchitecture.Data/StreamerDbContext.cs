daa67db65da8dcfc113af325fb1d9401f167a243
gomezcamilo9701 <gomezcamilo9701@gmail.com>
2023-10-04 15:12:39 -0500

    Adding dependencies and creating Data. (EF)

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        /** We should right click on dependecies and the "Add reference project"
+         * then we will be able to use <Streamer> and <Video>
+         * INSTALL EF SQL SERVER && EF TOOLS
+        */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source=localhost\sqlexpress; Inital Catalog=Streamer;Integrated Security=True");
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+
+
+    }
+}
---
+++ CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int StreamerId { get; set; } //FK
+        public virtual Stream? Streamer { get; set; }
+
+    }
+}
